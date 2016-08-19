ee2972acf3a98caa7ba12bb997b71cc45755ecc5
Daniel Tenbrinck <daniel.tenbrinck@wwu.de>
2016-08-19 14:26:22 +0200

    Changed normalization to be between 0 and 255

--- preprocessing/normalizeData.m
+++ preprocessing/normalizeData.m
@@ -1,5 +1,7 @@
 function normalized = normalizeData( data )
 
+maxi = 255.0;
+
 % if the input data is a struct we normalize each channel data set
 if isstruct(data)
 
@@ -8,20 +10,20 @@ if isstruct(data)
   
   % normalize Dapi channel
   Dapi = normalized.Dapi;
-  normalized.Dapi = ( Dapi - min(Dapi(:)) ) / ( max(Dapi(:)) - min(Dapi(:)) );
+  normalized.Dapi = ( Dapi - min(Dapi(:)) ) / ( max(Dapi(:)) - min(Dapi(:)) ) * maxi;
   
   % normalize GFP channel
   GFP = normalized.GFP;
-  normalized.GFP = ( GFP - min(GFP(:)) ) / ( max(GFP(:)) - min(GFP(:)) );
+  normalized.GFP = ( GFP - min(GFP(:)) ) / ( max(GFP(:)) - min(GFP(:)) ) * maxi;
   
   % normalize mCherry channel
   mCherry = normalized.mCherry;
-  normalized.mCherry = ( mCherry - min(mCherry(:)) ) / ( max(mCherry(:)) - min(mCherry(:)) );
+  normalized.mCherry = ( mCherry - min(mCherry(:)) ) / ( max(mCherry(:)) - min(mCherry(:)) ) * maxi;
   
 % otherwise we assume we normalize only one channel
 else
   
-  normalized = ( data - min(data(:)) ) / ( max(data(:)) - min(data(:)) );
+  normalized = ( data - min(data(:)) ) / ( max(data(:)) - min(data(:)) ) * maxi;
 
 end
 
