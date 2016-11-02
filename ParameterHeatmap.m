f39ac3377932c450551df9dec335945793aea21a
Fjedor Gaede <f_gaed01@wwu.de>
2016-11-02 20:04:37 +0100

    Added description to the parameters for the heatmap

--- ParameterHeatmap.m
+++ ParameterHeatmap.m
@@ -3,7 +3,7 @@ function p =  ParameterHeatmap()
 % This file contains all parameters needed for the heatmap script with
 % discriptios of each parameter. 
 
-%% PARAMETER 
+%% COMMON PARAMETER
 
 % -- SIZE OF CELL IN THE GRID DOMAIN -- %
 %  !!! Not working because not elaborated enough !!! %
@@ -16,23 +16,57 @@ p.sizeCellsPixel = round(p.sizeCells/p.sizeOfPixel);
 % -- PROCSESSING OF THE CELLS -- %
 % Tolerance for the cell to be outside of the sphere. Cells with norm
 % bigger than 1 but smaller than 1+tole will be normalized.
+% Default: 0.1
 p.tole = 0.1;
 
 % Grid size of the accumulator: 
 % size(accumulator) = [gridSize,gridSize,gridSize];
 p.gridSize = 256;
 
-% -- OPTIONS FOR THE HEATMAP HANDLER -- %
+%% OPTIONS FOR THE HEATMAP HANDLER -- %
+% The cellradius in pixels in the sampled p.gridSize space.
+% So this needs to be changed to depending on gridSize.
+% The cell will be cellradius*2 pixels big. 
+% !!! NEED A BETTER APPROACH !!! %
+% Default: 5 with gridSize = 256
 p.option.cellradius = 5;
+
+% - Slider options - %
+% Will show the Slider 
 p.option.slider = 1;  %in progress
 
-% - Options for the heatmap generation
-p.option.heatmaps.types = {'MIP','SUM'};
+% -- Options for the heatmap generation -- %
+
+% Process standard heatmaps? 1: yes, 0: no
+% Every option beneath will be irrelevant if 0.
 p.option.heatmaps.process = 1;
+
+% Types of heatmaps. Currently available 'MIP', 'SUM'.
+% Can handle multiple input.
+% Default: {'MIP','SUM'}
+p.option.heatmaps.types = {'MIP','SUM'};
+
+% - Saving - %
+% Saves will be aved in results folder under "heatmaps"
+% Save heatmaps
 p.option.heatmaps.save = 1;
+
+% Save heatmaps as file type
+% Can handle multiple input
+% Default: {'png','fig'}
+p.option.heatmaps.saveas = {'png'};
+
+% Save the heatmap structure with the matrices for the visualization in the
+% figures.
 p.option.heatmaps.saveHMmat = 1;
+
+% Save the accumulator matrix. This is the pure 3D matrix which stores the
+% number of cells at a voxel position
 p.option.heatmaps.saveAccumulator = 1;
-p.option.heatmaps.saveas = {'png','bmp'};
+
+% Show the heatmaps figures. Doesn't need to be activated for the saving.
 p.option.heatmaps.disp = 0;
-p.option.heatmaps.scaled = 'both'; % 'true','false','both'
+
+% Scale the heatmaps. Input can be: 'true','false','both'
+p.option.heatmaps.scaled = 'both';
 end
