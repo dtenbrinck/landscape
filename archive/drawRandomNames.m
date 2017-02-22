4549d755e2a2095ad2dc5732be125fb7b2c6b80c
Fjedor <f_gaed01@wwu.de>
2017-02-23 00:51:58 +0100

    Added a script that picks randomly different results from one experiment and compares them.

--- archive/drawRandomNames.m
+++ archive/drawRandomNames.m
@@ -1,7 +1,22 @@
-function [ fileNames ] = drawRandomNames( fileNames, numberOfRandom )
+function [ nameStack1, nameStack2 ] = drawRandomNames( fileNames, randomIn )
+
+if randomIn < 1
+    % Then it is percentage
+    numberOfRandom = round(size(fileNames,1)*randomIn);
+elseif randomIn >=1
+    % Then it is a number of picks
+    numberOfRandom = randomIn;
+end
 
 randIndices = sort(randsample(size(fileNames,1),numberOfRandom));
-fileNames = fileNames(randIndices);
+nameStack1 = fileNames(randIndices);
+fileNames(randIndices) = [];
+nameStack2 = fileNames;
 
+if nargout == 2
+    disp(['Divided stack of files into two random stacks of size ',num2str(numberOfRandom),' and ',num2str(size(nameStack2,1)),'.'])
+elseif nargout == 1 
+    disp([num2str(numberOfRandom),' files were randomly picked.'])
+end
 end
 
