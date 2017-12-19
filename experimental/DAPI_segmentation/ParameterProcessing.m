ec637a88bbba9c991a0e3d4bed495eee0a32f39e
ramona <sasser@uni-muenster.de>
2017-12-19 08:41:18 +0100

    use k-means method in dapi segmentation (experimental folder ready to test from biologists)

--- experimental/DAPI_segmentation/ParameterProcessing.m
+++ experimental/DAPI_segmentation/ParameterProcessing.m
@@ -2,8 +2,7 @@ function p =  ParameterProcessing()
 %% PARAMETERS PROCESSING SCRIPT
 % This file contains all parameters needed for the preocessing script with
 % discriptios of each parameter.        
-
-            
+  
 %% COMMON PARAMETER
 p.resolution = [1.29, 1.29, 20];
 p.scale = 0.75;
@@ -19,10 +18,9 @@ p.rmbg.GFPDiskSize = 50; %50
 p.rmbg.mCherryDiskSize = 11; %11
 
 %% SEGMENTATION
-
 % -- DAPI SEGMENTATION -- %
-p.DAPIseg.method = 'Laplacian'; % 'k-means', 'CP'
-%p.DAPIseg.k = 3;
+p.DAPIseg.method = 'k-means'; % 'Laplacian', 'CP'
+p.DAPIseg.k = 3;
 p.DAPIseg.maxNucleusSize = 20; % max. nucleus size
 p.DAPIseg.minNucleusSize = 3; % min. nucleus size
 % -- GFP SEGMENTATION -- %
@@ -33,10 +31,9 @@ p.GFPseg.morphSize = 5; %5
 p.mCherryseg.k = 3; %3
 p.mCherryseg.cellSize = 50; %50. in pixel
 p.mCherryseg.method = 'k-means'; % 'k-means', 'k-means_local', 'CP'
-
-%% REGISTRATION
 % -- LANDMARK PROJECTION -- %
 p.samples_sphere = 128;
+%% REGISTRATION
 % -- REGISTRATION OF LANDMARK -- %
 p.reg.landmarkCharacteristic = 'middle';
 p.reg.characteristicWeight = 0.5; %0,5
