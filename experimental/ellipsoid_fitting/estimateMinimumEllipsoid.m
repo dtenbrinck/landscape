45152885ce25b23b812419df2f28d1a42bf9ce67
ramona <sasser@uni-muenster.de>
2018-01-23 12:20:12 +0100

    wip minimization problem

--- experimental/ellipsoid_fitting/estimateMinimumEllipsoid.m
+++ experimental/ellipsoid_fitting/estimateMinimumEllipsoid.m
@@ -1,42 +1,40 @@
-function [ center, radii, evecs, v ] = estimateMinimumEllipsoid( X, equals )
+function [ center, radii, axes, v ] = estimateMinimumEllipsoid( X )
 %
 % Fit an ellispoid/sphere to a set of xyz data points:
 %
-%   [center, radii, evecs, pars ] = ellipsoid_fit( X )
-%   [center, radii, evecs, pars ] = ellipsoid_fit( [x y z] );
-%   [center, radii, evecs, pars ] = ellipsoid_fit( X, 1 );
-%   [center, radii, evecs, pars ] = ellipsoid_fit( X, 2, 'xz' );
-%   [center, radii, evecs, pars ] = ellipsoid_fit( X, 3 );
+%   [center, radii, axes, v ] = estimateMinimumEllipsoid( X )
+%   [center, radii, axes, v ] = estimateMinimumEllipsoid( [x y z] );
 %
 % Parameters:
 % * X, [x y z]   - Cartesian data, n x 3 matrix or three n x 1 vectors
-% * flag         - '' or empty fits an arbitrary ellipsoid (default),
-%                - 'xy' fits a spheroid with x- and y- radii equal
-%                - 'xz' fits a spheroid with x- and z- radii equal
-%                - 'xyz' fits a sphere
-%                - '0' fits an ellipsoid with its axes aligned along [x y z] axes
-%                - '0xy' the same with x- and y- radii equal
-%                - '0xz' the same with x- and z- radii equal
 %
 % Output:
-% * center    -  ellispoid or other conic center coordinates [xc; yc; zc]
-% * radii     -  ellipsoid or other conic radii [a; b; c]
-% * evecs     -  the radii directions as columns of the 3x3 matrix
-% * v         -  the 10 parameters describing the ellipsoid / conic algebraically: 
-%                Ax^2 + By^2 + Cz^2 + 2Dxy + 2Exz + 2Fyz + 2Gx + 2Hy + 2Iz + J = 0
-% * chi2      -  residual sum of squared errors (chi^2), this chi2 is in the 
-%                coordinate frame in which the ellipsoid is a unit sphere.
+% * center    -  ellispoid center coordinates [xc; yc; zc]
+%                     xc = - v_4 / ( 2 * v_1 )
+%                     yc = - v_5 / ( 2 * v_2 )
+%                     zc = - v_6 / ( 2 * v_3 )
+% * radii     -  ellipsoid radii [rx; ry; rz]
+%                     rx = v_1^-0.5 
+%                     ry = v_2^-0.5 
+%                     rz = v_3^-0.5
+% * axes      -  the radii directions as columns of the 3x3 matrix
+% * v         -  the 7 parameters describing the ellipsoid algebraically: 
+%                Ax^2 + By^2 + Cz^2 + Dx + Ey + Fz + G = 0
 %
+% Solving Minimization Problem to find smallest ellipsoid fitting leaving
+% only few data points outside
+%   sum(over all data points) H(< (v_1, ..., v_7) , (x^2, y^2, z^2, x, y, z, 1) >)
+%           + 4/3 * pi * v_1^-0.5 * v_2^-0.5 * v_3^-0.5
+%
+%   first summand: smeared heaviside function of evaluated ellipsoid equation 
+%                   in all data points; only includes distances of data 
+%                   points outside of ellipsoid
+%   second summand: volume of ellipsoid
 % Author:
-% Yury Petrov, Oculus VR
+% Ramona Sasse
 % Date:
-% September, 2015
+% January, 2018
 %
-
-narginchk( 1, 3 ) ;  % check input arguments
-if nargin == 1
-    equals = ''; % no constraints by default
-end
     
 if size( X, 2 ) ~= 3
     error( 'Input data must have three columns!' );
@@ -46,85 +44,26 @@ else
     z = X( :, 3 );
 end
 
-% need nine or more data points
-if length( x ) < 9 && strcmp( equals, '' ) 
-   error( 'Must have at least 9 points to fit a unique ellipsoid' );
+% need 7?! or more data points
+if length( x ) < 7
+   error( 'Must have at least 7 points to fit a unique ellipsoid' );
 end
-if length( x ) < 8 && ( strcmp( equals, 'xy' ) || strcmp( equals, 'xz' ) )
-   error( 'Must have at least 8 points to fit a unique ellipsoid with two equal radii' );
-end
-if length( x ) < 6 && strcmp( equals, '0' )
-   error( 'Must have at least 6 points to fit a unique oriented ellipsoid' );
-end
-if length( x ) < 5 && ( strcmp( equals, '0xy' ) || strcmp( equals, '0xz' ) )
-   error( 'Must have at least 5 points to fit a unique oriented ellipsoid with two equal radii' );
-end
-if length( x ) < 4 && strcmp( equals, 'xyz' );
-   error( 'Must have at least 4 points to fit a unique sphere' );
-end
+
 
 % fit ellipsoid in the form Ax^2 + By^2 + Cz^2 + 2Dxy + 2Exz + 2Fyz + 2Gx +
 % 2Hy + 2Iz + J = 0 and A + B + C = 3 constraint removing one extra
 % parameter
-if strcmp( equals, '' )
-    D = [ x .* x + y .* y - 2 * z .* z, ...
-        x .* x + z .* z - 2 * y .* y, ...
-        2 * x .* y, ...
-        2 * x .* z, ...
-        2 * y .* z, ...
-        2 * x, ...
-        2 * y, ...
-        2 * z, ...
-        1 + 0 * x ];  % ndatapoints x 9 ellipsoid parameters
-elseif strcmp( equals, 'xy' )
-    D = [ x .* x + y .* y - 2 * z .* z, ...
-        2 * x .* y, ...
-        2 * x .* z, ...
-        2 * y .* z, ...
-        2 * x, ...
-        2 * y, ...
-        2 * z, ...
-        1 + 0 * x ];  % ndatapoints x 8 ellipsoid parameters
-elseif strcmp( equals, 'xz' )
-    D = [ x .* x + z .* z - 2 * y .* y, ...
-        2 * x .* y, ...
-        2 * x .* z, ...
-        2 * y .* z, ...
-        2 * x, ...
-        2 * y, ...
-        2 * z, ...
-        1 + 0 * x ];  % ndatapoints x 8 ellipsoid parameters
-    % fit ellipsoid in the form Ax^2 + By^2 + Cz^2 + 2Gx + 2Hy + 2Iz = 1
-elseif strcmp( equals, '0' )
-    D = [ x .* x + y .* y - 2 * z .* z, ...
-          x .* x + z .* z - 2 * y .* y, ...
-          2 * x, ...
-          2 * y, ... 
-          2 * z, ... 
-          1 + 0 * x ];  % ndatapoints x 6 ellipsoid parameters
-    % fit ellipsoid in the form Ax^2 + By^2 + Cz^2 + 2Gx + 2Hy + 2Iz = 1,
-    % where A = B or B = C or A = C
-elseif strcmp( equals, '0xy' )
-    D = [ x .* x + y .* y - 2 * z .* z, ...
-          2 * x, ...
-          2 * y, ... 
-          2 * z, ... 
-          1 + 0 * x ];  % ndatapoints x 5 ellipsoid parameters
-elseif strcmp( equals, '0xz' )
-    D = [ x .* x + z .* z - 2 * y .* y, ...
-          2 * x, ...
-          2 * y, ... 
-          2 * z, ... 
-          1 + 0 * x ];  % ndatapoints x 5 ellipsoid parameters
-     % fit sphere in the form A(x^2 + y^2 + z^2) + 2Gx + 2Hy + 2Iz = 1
-elseif strcmp( equals, 'xyz' )
-    D = [ 2 * x, ...
-          2 * y, ... 
-          2 * z, ... 
-          1 + 0 * x ];  % ndatapoints x 4 ellipsoid parameters
-else
-    error( [ 'Unknown parameter value ' equals '!' ] );
-end
+
+%     D = [ x .* x + y .* y - 2 * z .* z, ...
+%         x .* x + z .* z - 2 * y .* y, ...
+%         2 * x .* y, ...
+%         2 * x .* z, ...
+%         2 * y .* z, ...
+%         2 * x, ...
+%         2 * y, ...
+%         2 * z, ...
+%         1 + 0 * x ];  % ndatapoints x 9 ellipsoid parameters
+
 
 % solve the normal system of equations
 d2 = x .* x + y .* y + z .* z; % the RHS of the llsq problem (y's)
