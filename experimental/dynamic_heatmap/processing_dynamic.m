b473968b7d55d7a76925fa6fc5c672ec4be81fd3
Daniel Tenbrinck <daniel.tenbrinck@wwu.de>
2018-06-27 14:12:39 +0200

    Extracting and registering PGC features is working.

--- experimental/dynamic_heatmap/processing_dynamic.m
+++ experimental/dynamic_heatmap/processing_dynamic.m
@@ -1,3 +1,6 @@
+% for each dynamic feature, e.g., velocity or track straightness, we
+% assume the value has been determined with respect to the next timepoint t+1
+
 %% INITIALIZATION
 clear; clc; close all;
 
@@ -24,36 +27,27 @@ p.resolution(1:2) = p.resolution(1:2) / p.scale;
 
 % get list of subdirectories in which the data resides
 dirContent = dir(p.dataPath);
-numSubdirs = sum([dirContent(~ismember({dirContent.name},{'.','..'})).isdir]);
+numberSubdirs = sum([dirContent(~ismember({dirContent.name},{'.','..'})).isdir]);
 listSubdirs = dirContent(~ismember({dirContent.name},{'.','..'}) & [dirContent.isdir]);
-namesSubdirs = cell(numSubdirs,1);
-for i=1:numSubdirs
+namesSubdirs = cell(numberSubdirs,1);
+for i=1:numberSubdirs
     namesSubdirs{i} = listSubdirs(i).name;
 end
 
-% % processing all data is set to default
-% process_data = ones(1, numSubdirs);
-% 
-% % check if a common transformation is wanted and determine reference timepoint dataset
-% if p.commonTransformation == true
-%     midtime = round(numSubdirs/2);
-%     process_data = ~process_data;
-%     process_data(midtime) = 1;
-% end
-% 
-% % extract experiment data numbers to be processed
-% num_process_data = find(process_data);
-
 
 %% LOOP OVER ALL DATA DIRECTORIES
+%%% WE ASSUME THAT EACH DYNAMIC DATA SET IS IN IT'S OWN SUBDIRECTORY
 
-% loop over all experiments that should be processed
-for i=numSubdirs
+% initialize struct array to save dynamic information for all PGCs
+global_PGC_information = struct([]);
+
+% loop over all subdirectories each containing one dynamic data set
+for i=1:numberSubdirs
     path_data = [p.dataPath '/' namesSubdirs{i}];
     
     fileNamesTIF = getTIFfilenames(path_data);
     
-    % get data of current experiment
+    % get static reference data of current data set
     if p.debug_level >= 1; disp('Loading data...'); end
     BFP = loadStaticTIF([path_data '/' fileNamesTIF{1}]); % load BFP
     GFP = loadStaticTIF([path_data '/' fileNamesTIF{2}]); % load GFP
@@ -105,10 +99,7 @@ for i=numSubdirs
         visualizeProjectedLandmark(sphereCoordinates, landmarkOnSphere);
         visualizeProjectedLandmark(registered_sphere', landmarkOnSphere);
     end
-    
-    %        % DEBUG
-    %        transformedCoordinates = rotationMatrix * landmarkCoordinates';
-    
+      
     % compute registration transformation from original data space
     transformation_registration = transformationMatrix * rotationMatrix';
     
@@ -122,19 +113,57 @@ for i=numSubdirs
     tmp = load([path_data '/' fileNamesMAT{fileIndex}]);
     PGC_tracks = tmp.tracks_PGC;
     
-    % register data
-    if p.debug_level >= 1; disp('Registering data...'); end
-    registeredData = registerData( processedData, p.resolution, transformation_registration, ellipsoid, p.samples_cube);
+    % determine number of tracked PGCs for this data set
+    numberPGCs = size(PGC_tracks,1);
+ 
+    % initialize struct array to save dynamic information for this data set
+    PGC_information = struct([]);
     
-    % create filename to save results
-    results_filename = [p.resultsPath '/' experimentData.filename '_results.mat'];
-    
-    gatheredData = saveResults(experimentData, processedData, registeredData, ellipsoid, transformationMatrix, rotationMatrix, results_filename);
-    
-    % visualize results if needed
-    if p.visualization == 1
-        visualizeResults_new(gatheredData);
+    % loop over every tracked PGC
+    for j=1:numberPGCs
+        
+        % determine number of time points this PGC has been detected
+        numberTimepoints = size(PGC_tracks{j},1);
+        
+        %% extract dynamic features of tracked PGCs
+        % WE ASSUME: originalPositions(2,:) - originalPositions(1,:) = distance(1,:)
+        
+        % extract original position of PGC at time point t as (x,y,z) coordinates
+        originalPositions = PGC_tracks{j}(:,2:4);
+        
+        % transform PGC coordinates into reference system
+        PGC_information(j).registeredPositions = transformCoordinates(originalPositions, ellipsoid.center, transformation_registration^-1, [0; 0; 0]);
+        
+        % extract time interval between timepoints
+        deltaT = PGC_tracks{j}(:,5);
+        
+        % extract traveled distance of PGC in mumeter as (x,y,z) vector
+        distance = PGC_tracks{j}(:,6:8);
+        
+        % extract velocity of PGC in mumeter/timeinterval as (x,y,z) vector
+        velocity = PGC_tracks{j}(:,9:11);
+        
+        %%% SANITY CHECK: Check if velocity is distance / deltaT
+        if p.debug_level >= 2
+           assert(isequal(velocity, distance./repmat(deltaT,1,3)));
+        end
+        
+        % extract background velocity in mumeter/timeinterval as (x,y,z) vector
+        background_velocity = PGC_tracks{j}(:,12:14);
+        
+        % compute net velocity of PGC by subtracting background velocity
+        PGC_information(j).net_velocity = (velocity - background_velocity)';
+        
     end
+    
+    % concatenate PGC information from this data set with global struct array
+    global_PGC_information = cat(2, global_PGC_information, PGC_information);
+        
+    % create filename to save results
+    results_filename = [p.resultsPath '/dynamic_features.mat'];
+    
+    % save collected dynamic features of all tracked PGCs and transformation matrix
+    save(results_filename,'global_PGC_information', 'transformation_registration');
     
     if p.debug_level >= 1; disp('Saved results successfully!'); end
 end
