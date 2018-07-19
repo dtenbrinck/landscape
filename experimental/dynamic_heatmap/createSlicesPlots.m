3684f008620f9ce26f652e25998661108a03443c
ramona <sasser@uni-muenster.de>
2018-07-19 14:45:05 +0200

    first try to plot surf plot for single slices

--- experimental/dynamic_heatmap/createSlicesPlots.m
+++ experimental/dynamic_heatmap/createSlicesPlots.m
@@ -19,6 +19,33 @@ function createSlicesPlots(accumulator, option)
 %     axis equal;colorbar;
 
     figure; colormap jet;
-    contourslice(convAcc(:,:,1:160), [],[], 0:20:160);
-    colorbar;
+    subplot(1,2,1);
+    contourslice(convAcc(:,:,1:160), [],[], 0:10:160);
+    colorbar; view(3);
+    hold on;
+    [x,y,z] = sphere;
+    surf(128*x + 128, 128*y + 128, 128*z+ 128,'FaceAlpha',0.1, 'FaceColor', 'm', 'EdgeColor', 'none');
+    title('contour slices with transparent spherical surface');
+    subplot(1,2,2);
+    contourslice(convAcc(:,:,1:160), [],[], 0:10:160);
+    colorbar; view(3);
+    
+%     for i=1:10
+%         figure
+%         subplot(2,1,1)
+%         contour(convAcc(:,:,(10*(i-1)+1):10*i));
+%         axis equal
+%         title('Contour Plot on Tilted Plane')
+%         xlabel('x')
+%         ylabel('y')
+%         colorbar
+%         subplot(2,1,2)
+%         surf(convAcc(:,:,(10*(i-1)+1):10*i),'LineStyle','none');
+%         axis equal
+%         view(0,90)
+%         title('Colored Plot on Tilted Plane')
+%         xlabel('x')
+%         ylabel('y')
+%         colorbar
+%     end
 end
