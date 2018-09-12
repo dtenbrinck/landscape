c3fdd461ddf6a6f3c4c067a7b4bbfd2ea1b04e3a
ramona <sasser@uni-muenster.de>
2018-09-12 13:08:51 +0200

    corrected plotting and trying to plot only subset to make arrows visible

--- experimental/only_GFP_DAPI_input/plotEllipsoidAndNucleiAfterRotation.m
+++ experimental/only_GFP_DAPI_input/plotEllipsoidAndNucleiAfterRotation.m
@@ -2,6 +2,10 @@ function plotEllipsoidAndNucleiAfterRotation (nucleiOrigCoord, ellipsoidEstimati
     X(:,1) = (nucleiOrigCoord(1,:) * resolution(1))';
     X(:,2) = (nucleiOrigCoord(2,:) * resolution(2))';
     X(:,3) = (nucleiOrigCoord(3,:) * resolution(3))';
+    rng(0,'twister');
+    idx = randperm( size(X,1), ceil(10/100*size(X,1)));
+    X = X(idx,:);
+
     center = ellipsoidEstimation.center;
     axes = ellipsoidEstimation.axes;
     n = 20;
@@ -9,7 +13,7 @@ function plotEllipsoidAndNucleiAfterRotation (nucleiOrigCoord, ellipsoidEstimati
     
     fprintf("Plotting resulting ellipsoid estimation...\n");
     rotatedCoordinates = axes' * [reshape(x_ellipsoid,1,n*n); reshape(y_ellipsoid,1,n*n); reshape(z_ellipsoid,1,n*n)];
-    [x,y,z] = reshapeXYZ(rotatedCoordinates, center);
+    [x,y,z] = reshapeXYZ(rotatedCoordinates, center, n);
     
     figure('Units', 'normalized', 'Position', [0.1 0.1 0.75 0.6]);
     subplot(1,3,1);
@@ -24,7 +28,7 @@ function plotEllipsoidAndNucleiAfterRotation (nucleiOrigCoord, ellipsoidEstimati
     X = (rotationMatrix' * X')';
     center = rotationMatrix' * ellipsoidEstimation.center;
     rotatedCoordinates = rotationMatrix' * axes' * [reshape(x_ellipsoid,1,n*n); reshape(y_ellipsoid,1,n*n); reshape(z_ellipsoid,1,n*n)];
-    [x,y,z] = reshapeXYZ(rotatedCoordinates, center);
+    [x,y,z] = reshapeXYZ(rotatedCoordinates, center, n);
     subplot(1,3,2);
     hold on;
     plotEllipsoids(X, x,y,z);
@@ -37,12 +41,12 @@ function plotEllipsoidAndNucleiAfterRotation (nucleiOrigCoord, ellipsoidEstimati
     X(:,3) = ((nucleiOrigCoord(3,:) * resolution(3))' - ellipsoidEstimation.center(3) );
     X = ((transformationMatrix * rotationMatrix')\  X')';
     rotatedCoordinates = (transformationMatrix * rotationMatrix' )\ ( axes' * [reshape(x_ellipsoid,1,n*n); reshape(y_ellipsoid,1,n*n); reshape(z_ellipsoid,1,n*n)]);
-    [x,y,z] = reshapeXYZ(rotatedCoordinates, [0,0,0]);
+    [x,y,z] = reshapeXYZ(rotatedCoordinates, [0,0,0], n);
     
     subplot(1,3,3);
     hold on;
     plotEllipsoids(X, x,y,z);
-    plotOrientationVectors( [0,0,0], ((transformationMatrix * rotationMatrix' )\ axes')', [1 1 1]);
+    plotOrientationVectors( [0,0,0], ((transformationMatrix * rotationMatrix' )\ axes')',  ellipsoidEstimation.radiiOrderedForPlots);
     title(['scaled and rotated ellipsoid estimation'], 'Interpreter', 'tex'); 
     
 
@@ -58,7 +62,7 @@ function plotEllipsoids(X, x,y,z)
     legend('Location', 'southoutside');
 end
 
-function [x,y,z] = reshapeXYZ(rotatedCoordinates, center)
+function [x,y,z] = reshapeXYZ(rotatedCoordinates, center, n)
     x = reshape(rotatedCoordinates(1, : ), [n, n]);
     y = reshape(rotatedCoordinates(2, : ), [n, n]);
     z = reshape(rotatedCoordinates(3, : ), [n, n]);
@@ -68,7 +72,6 @@ function [x,y,z] = reshapeXYZ(rotatedCoordinates, center)
 end
 
 function plotOrientationVectors( center, axis, radii)
-    axis= 100*axis;
     quiver3( center(1), center(2), center(3), radii(1)*axis(1,1), radii(1)*axis(2,1), radii(1)*axis(3,1), 'k', 'LineWidth', 2, 'Displayname','first axis'); hold on;
     quiver3( center(1), center(2), center(3), radii(2)*axis(1,2), radii(2)*axis(2,2), radii(2)*axis(3,2), 'r', 'LineWidth', 2, 'Displayname','second axis');hold on;
     quiver3( center(1), center(2), center(3), radii(3)*axis(1,3), radii(3)*axis(2,3), radii(3)*axis(3,3), 'g', 'LineWidth', 2, 'Displayname','third axis');
