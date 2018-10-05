59abf9ddd1967dc941486b3e4444b82737328db4
Ramona <sasser@uni-muenster.de>
2018-10-05 09:43:33 +0100

    changed parameter for new ellipsoid estimation in mutant processing script

--- experimental/mutant_registration/processing.m
+++ experimental/mutant_registration/processing.m
@@ -62,7 +62,7 @@ for experiment=1:numberOfExperiments
 
         % estimate embryo surface by fitting an ellipsoid
         if p.debug_level >= 1; disp('Estimating embryo surface...'); end
-        ellipsoid = estimateEmbryoSurface(processedData.nucleiCoordinates, p.resolution);
+        ellipsoid = estimateEmbryoSurface(processedData.nucleiCoordinates, p.resolution, p.ellipsoidFitting);
         
         % compute transformation which normalizes the estimated ellipsoid to a unit sphere
         if p.debug_level >= 1; disp('Compute transformation from optimal ellipsoid...'); end
