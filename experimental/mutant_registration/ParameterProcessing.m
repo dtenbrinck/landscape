ecce104e9d19fe5165ced648e610e7e06593d15c
Ramona <45093700+zebra-fish@users.noreply.github.com>
2019-03-04 22:32:04 +0100

    changed parameters for ellipsoid estimation: better approx than the very underestimated ellipsoid due to fewer data points from dapi segmentation now

--- experimental/mutant_registration/ParameterProcessing.m
+++ experimental/mutant_registration/ParameterProcessing.m
@@ -22,6 +22,7 @@ p.rmbg.mCherryDiskSize = 11; %11
 % -- DAPI SEGMENTATION -- %
 p.DAPIseg.method = 'k-means'; % 'Laplacian', 'CP'
 p.DAPIseg.k = 3;
+p.DAPIseg.minNucleusSize = 6; % min. nucleus size
 % -- GFP SEGMENTATION -- %
 p.GFPseg.k = 4; %4 
 p.GFPseg.morphSize = 5; %5
@@ -33,7 +34,7 @@ p.GFPseg.threshold = 0; % SD: 0, EPI: 0.98 e.g.
 % default: 'kittler'
 % 'k-means': Spinning Disc (SD) Confocal Microscopy data
 % 'kittler': Epifluorescence Microscopy (EPI): kittler
-p.mCherryseg.binarization = 'kittler'; % 'k-means', 'kittler'
+p.mCherryseg.binarization = 'k-means'; % 'k-means', 'kittler'
 p.mCherryseg.k = 3; %3
 p.mCherryseg.cellSize = 50; %50. in pixel
 p.mCherryseg.method = 'k-means'; % 'k-means', 'k-means_local', 'CP'
@@ -43,8 +44,8 @@ p.samples_sphere = 128;
 %% ELLIPSOIDAL FITTING
 p.ellipsoidFitting.percentage = 10;
 p.ellipsoidFitting.visualization = 0;
-p.ellipsoidFitting.regularisationParams.mu0 = 10^-8;
-p.ellipsoidFitting.regularisationParams.mu1 = 0.002; 
+p.ellipsoidFitting.regularisationParams.mu0 = 10^-7;
+p.ellipsoidFitting.regularisationParams.mu1 = 2*10^-4; 
 p.ellipsoidFitting.regularisationParams.mu2 = 1;
 p.ellipsoidFitting.regularisationParams.gamma = 1; 
 p.ellipsoidFitting.descentMethod = 'cg'; % 'grad'
