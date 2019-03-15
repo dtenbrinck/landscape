78d200687fada36a766212ef8639d891c88d11e2
JanHuewel <47003263+JanHuewel@users.noreply.github.com>
2019-03-15 16:48:50 +0100

    set the parameter for saving ellipsoid estimations

--- parameter_setup/ParameterProcessing.m
+++ parameter_setup/ParameterProcessing.m
@@ -42,8 +42,8 @@ p.mCherryseg.method = 'k-means'; % 'k-means', 'k-means_local', 'CP'
 p.samples_sphere = 128;
 
 %% ELLIPSOIDAL FITTING
-p.ellipsoidFitting.percentage = 10;
-p.ellipsoidFitting.visualization = 0;
+p.ellipsoidFitting.percentage = 100;
+p.ellipsoidFitting.visualization = 1;
 p.ellipsoidFitting.regularisationParams.mu0 = 10^-7;
 p.ellipsoidFitting.regularisationParams.mu1 = 2*10^-4; 
 p.ellipsoidFitting.regularisationParams.mu2 = 1;
