f0d3cfcd1587e1e0043f692078e8ee1a78c61423
Daniel Tenbrinck <daniel.tenbrinck@fau.de>
2020-01-04 10:04:11 +0100

    Added parameter for different shells to generateHeatmaps.m

--- heatmaps/generateHeatmaps.m
+++ heatmaps/generateHeatmaps.m
@@ -56,145 +56,148 @@ mCherry_accumulator = computeAccumulator(PGC_coords_grid, p.gridSize);
 % --- Generate shells with valid cell coordinates per shell
 [minRadius, maxRadius] = computeLandmarkShell(p, fileNames, numberOfResults);
 
-%% NEW APPROACH: SHELLS WITH EQUAL SIZE
-
-% compute thickness of landmark shell
-shellThickness = abs(maxRadius - minRadius);
-
-% extract landmark shells
-GFP_landmarkShell = getShell(landmark_coords_unitsphere, minRadius, maxRadius);
-DAPI_landmarkShell = getShell(nuclei_coords_unitsphere, minRadius, maxRadius);
-mCherry_landmarkShell = getShell(PGC_coords_unitsphere, minRadius, maxRadius);
-
-% initialize auxiliary variables
-numberOfShellsExtracted = 0;
-shellsEmpty = false;
-currentRadius = maxRadius;
-
-GFP_aboveShells = cell(0,1);
-DAPI_aboveShells = cell(0,1);
-mCherry_aboveShells = cell(0,1);
-
-% extract shells above landmark
-while shellsEmpty == false && numberOfShellsExtracted < 25
+if p.equalSizedShells == true
+    %% NEW APPROACH: SHELLS WITH EQUAL SIZE
     
-    % update counter variable
-    numberOfShellsExtracted = numberOfShellsExtracted + 1;
+    % compute thickness of landmark shell
+    shellThickness = abs(maxRadius - minRadius);
     
-    % extract shells
-    GFP_aboveShells{numberOfShellsExtracted} = getShell(landmark_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
-    DAPI_aboveShells{numberOfShellsExtracted} = getShell(nuclei_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
-    mCherry_aboveShells{numberOfShellsExtracted} = getShell(PGC_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
+    % extract landmark shells
+    GFP_landmarkShell = getShell(landmark_coords_unitsphere, minRadius, maxRadius);
+    DAPI_landmarkShell = getShell(nuclei_coords_unitsphere, minRadius, maxRadius);
+    mCherry_landmarkShell = getShell(PGC_coords_unitsphere, minRadius, maxRadius);
     
-    % update currentRadius
-    currentRadius = currentRadius + shellThickness;
+    % initialize auxiliary variables
+    numberOfShellsExtracted = 0;
+    shellsEmpty = false;
+    currentRadius = maxRadius;
     
-    % check if all shells are empty (outside of region of interest)
-    if sum(GFP_aboveShells{numberOfShellsExtracted}(:)) + sum(DAPI_aboveShells{numberOfShellsExtracted}(:)) + sum(mCherry_aboveShells{numberOfShellsExtracted}(:)) == 0
-       shellsEmpty = true; 
-    end
-end
-
-% give some output
-disp(['Number of above shells extracted: ' num2str(numberOfShellsExtracted)]);
-
-% reinitialize auxiliary variables
-numberOfShellsExtracted = 0;
-shellsEmpty = false;
-currentRadius = minRadius;
-
-GFP_belowShells = cell(0,1);
-DAPI_belowShells = cell(0,1);
-mCherry_belowShells = cell(0,1);
-
-% extract shells above landmark
-while shellsEmpty == false && numberOfShellsExtracted < 25
+    GFP_aboveShells = cell(0,1);
+    DAPI_aboveShells = cell(0,1);
+    mCherry_aboveShells = cell(0,1);
     
-    % update counter variable
-    numberOfShellsExtracted = numberOfShellsExtracted + 1;
-    
-    % make sure we stay positive
-    if currentRadius - shellThickness < 0
-        shellThickness = currentRadius;
-        shellsEmpty = true;
+    % extract shells above landmark
+    while shellsEmpty == false && numberOfShellsExtracted < 25
+        
+        % update counter variable
+        numberOfShellsExtracted = numberOfShellsExtracted + 1;
+        
+        % extract shells
+        GFP_aboveShells{numberOfShellsExtracted} = getShell(landmark_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
+        DAPI_aboveShells{numberOfShellsExtracted} = getShell(nuclei_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
+        mCherry_aboveShells{numberOfShellsExtracted} = getShell(PGC_coords_unitsphere, currentRadius+eps, currentRadius + shellThickness);
+        
+        % update currentRadius
+        currentRadius = currentRadius + shellThickness;
+        
+        % check if all shells are empty (outside of region of interest)
+        if sum(GFP_aboveShells{numberOfShellsExtracted}(:)) + sum(DAPI_aboveShells{numberOfShellsExtracted}(:)) + sum(mCherry_aboveShells{numberOfShellsExtracted}(:)) == 0
+            shellsEmpty = true;
+        end
     end
     
-    % extract shells
-    GFP_belowShells{numberOfShellsExtracted} = getShell(landmark_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
-    DAPI_belowShells{numberOfShellsExtracted} = getShell(nuclei_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
-    mCherry_belowShells{numberOfShellsExtracted} = getShell(PGC_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
+    % give some output
+    disp(['Number of above shells extracted: ' num2str(numberOfShellsExtracted)]);
     
-    % update currentRadius
-    currentRadius = currentRadius - shellThickness;
+    % reinitialize auxiliary variables
+    numberOfShellsExtracted = 0;
+    shellsEmpty = false;
+    currentRadius = minRadius;
     
-    % check if all shells are empty (outside of region of interest)
-    if sum(GFP_belowShells{numberOfShellsExtracted}(:)) + sum(DAPI_belowShells{numberOfShellsExtracted}(:)) + sum(mCherry_belowShells{numberOfShellsExtracted}(:)) == 0
-       shellsEmpty = true; 
+    GFP_belowShells = cell(0,1);
+    DAPI_belowShells = cell(0,1);
+    mCherry_belowShells = cell(0,1);
+    
+    % extract shells above landmark
+    while shellsEmpty == false && numberOfShellsExtracted < 25
+        
+        % update counter variable
+        numberOfShellsExtracted = numberOfShellsExtracted + 1;
+        
+        % make sure we stay positive
+        if currentRadius - shellThickness < 0
+            shellThickness = currentRadius;
+            shellsEmpty = true;
+        end
+        
+        % extract shells
+        GFP_belowShells{numberOfShellsExtracted} = getShell(landmark_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
+        DAPI_belowShells{numberOfShellsExtracted} = getShell(nuclei_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
+        mCherry_belowShells{numberOfShellsExtracted} = getShell(PGC_coords_unitsphere, currentRadius - shellThickness, currentRadius-eps);
+        
+        % update currentRadius
+        currentRadius = currentRadius - shellThickness;
+        
+        % check if all shells are empty (outside of region of interest)
+        if sum(GFP_belowShells{numberOfShellsExtracted}(:)) + sum(DAPI_belowShells{numberOfShellsExtracted}(:)) + sum(mCherry_belowShells{numberOfShellsExtracted}(:)) == 0
+            shellsEmpty = true;
+        end
     end
-end
-
-% give some output
-disp(['Number of below shells extracted: ' num2str(numberOfShellsExtracted)]);
-
-% generate shells
-GFP_shells = cell(0,1);
-DAPI_shells = cell(0,1);
-mCherry_shells = cell(0,1);
-
-% above shells are saved in wrong order so we need to invert the order here
-numberOfShells = 1;
-for currentShell = size(GFP_aboveShells,2):-1:1
-    GFP_shells{numberOfShells} = GFP_aboveShells{currentShell};
-    DAPI_shells{numberOfShells} = DAPI_aboveShells{currentShell};
-    mCherry_shells{numberOfShells} = mCherry_aboveShells{currentShell};
+    
+    % give some output
+    disp(['Number of below shells extracted: ' num2str(numberOfShellsExtracted)]);
+    
+    % generate shells
+    GFP_shells = cell(0,1);
+    DAPI_shells = cell(0,1);
+    mCherry_shells = cell(0,1);
+    
+    % above shells are saved in wrong order so we need to invert the order here
+    numberOfShells = 1;
+    for currentShell = size(GFP_aboveShells,2):-1:1
+        GFP_shells{numberOfShells} = GFP_aboveShells{currentShell};
+        DAPI_shells{numberOfShells} = DAPI_aboveShells{currentShell};
+        mCherry_shells{numberOfShells} = mCherry_aboveShells{currentShell};
+        numberOfShells = numberOfShells + 1;
+    end
+    
+    % save landmark shell
+    GFP_shells{numberOfShells} = GFP_landmarkShell;
+    DAPI_shells{numberOfShells} = DAPI_landmarkShell;
+    mCherry_shells{numberOfShells} = mCherry_landmarkShell;
     numberOfShells = numberOfShells + 1;
+    
+    % below shells are already in right order
+    for currentShell = 1:size(GFP_belowShells,2)
+        GFP_shells{numberOfShells} = GFP_belowShells{currentShell};
+        DAPI_shells{numberOfShells} = DAPI_belowShells{currentShell};
+        mCherry_shells{numberOfShells} = mCherry_belowShells{currentShell};
+        numberOfShells = numberOfShells + 1;
+    end
+    
+else
+    %% OLD APPROACH: EXTRACT THREE SHELLS - LANDMARK, ABOVE, BELOW
+    GFP_landmarkShell = getShell(landmark_coords_unitsphere, minRadius, maxRadius);
+    GFP_aboveLandmark = getShell(landmark_coords_unitsphere, maxRadius+eps, Inf);
+    GFP_belowLandmark = getShell(landmark_coords_unitsphere, 0, minRadius-eps);
+    
+    GFP_shells = cell(0,1);
+    GFP_shells{1} = GFP_aboveLandmark;
+    GFP_shells{2} = GFP_landmarkShell;
+    GFP_shells{3} = GFP_belowLandmark;
+    GFP_shells{4} = getShell(landmark_coords_unitsphere,0,Inf);
+    
+    DAPI_landmarkShell = getShell(nuclei_coords_unitsphere, minRadius, maxRadius);
+    DAPI_aboveLandmark = getShell(nuclei_coords_unitsphere, maxRadius+eps, Inf);
+    DAPI_belowLandmark = getShell(nuclei_coords_unitsphere, 0, minRadius-eps);
+    
+    DAPI_shells = cell(0,1);
+    DAPI_shells{1} = DAPI_aboveLandmark;
+    DAPI_shells{2} = DAPI_landmarkShell;
+    DAPI_shells{3} = DAPI_belowLandmark;
+    DAPI_shells{4} = getShell(nuclei_coords_unitsphere,0,Inf);
+    
+    mCherry_landmarkShell = getShell(PGC_coords_unitsphere, minRadius, maxRadius);
+    mCherry_aboveLandmark = getShell(PGC_coords_unitsphere, maxRadius+eps, Inf);
+    mCherry_belowLandmark = getShell(PGC_coords_unitsphere, 0, minRadius-eps);
+    
+    mCherry_shells = cell(0,1);
+    mCherry_shells{1} = mCherry_aboveLandmark;
+    mCherry_shells{2} = mCherry_landmarkShell;
+    mCherry_shells{3} = mCherry_belowLandmark;
+    mCherry_shells{4} = getShell(PGC_coords_unitsphere,0,Inf);
+    
 end
-
-% save landmark shell
-GFP_shells{numberOfShells} = GFP_landmarkShell;
-DAPI_shells{numberOfShells} = DAPI_landmarkShell;
-mCherry_shells{numberOfShells} = mCherry_landmarkShell;
-numberOfShells = numberOfShells + 1;
-
-% below shells are already in right order
-for currentShell = 1:size(GFP_belowShells,2)
-    GFP_shells{numberOfShells} = GFP_belowShells{currentShell};
-    DAPI_shells{numberOfShells} = DAPI_belowShells{currentShell};
-    mCherry_shells{numberOfShells} = mCherry_belowShells{currentShell};
-    numberOfShells = numberOfShells + 1;
-end
-
-%% OLD APPROACH: EXTRACT THREE SHELLS - LANDMARK, ABOVE, BELOW
-% GFP_landmarkShell = getShell(landmark_coords_unitsphere, minRadius, maxRadius);
-% GFP_aboveLandmark = getShell(landmark_coords_unitsphere, maxRadius+eps, Inf);
-% GFP_belowLandmark = getShell(landmark_coords_unitsphere, 0, minRadius-eps);
-% 
-% GFP_shells = cell(0,1);
-% GFP_shells{1} = GFP_aboveLandmark;
-% GFP_shells{2} = GFP_landmarkShell;
-% GFP_shells{3} = GFP_belowLandmark;
-% GFP_shells{4} = getShell(landmark_coords_unitsphere,0,Inf);
-% 
-% DAPI_landmarkShell = getShell(nuclei_coords_unitsphere, minRadius, maxRadius);
-% DAPI_aboveLandmark = getShell(nuclei_coords_unitsphere, maxRadius+eps, Inf);
-% DAPI_belowLandmark = getShell(nuclei_coords_unitsphere, 0, minRadius-eps);
-% 
-% DAPI_shells = cell(0,1);
-% DAPI_shells{1} = DAPI_aboveLandmark;
-% DAPI_shells{2} = DAPI_landmarkShell;
-% DAPI_shells{3} = DAPI_belowLandmark;
-% DAPI_shells{4} = getShell(nuclei_coords_unitsphere,0,Inf);
-% 
-% mCherry_landmarkShell = getShell(PGC_coords_unitsphere, minRadius, maxRadius);
-% mCherry_aboveLandmark = getShell(PGC_coords_unitsphere, maxRadius+eps, Inf);
-% mCherry_belowLandmark = getShell(PGC_coords_unitsphere, 0, minRadius-eps);
-% 
-% mCherry_shells = cell(0,1);
-% mCherry_shells{1} = mCherry_aboveLandmark;
-% mCherry_shells{2} = mCherry_landmarkShell;
-% mCherry_shells{3} = mCherry_belowLandmark;
-% mCherry_shells{4} = getShell(PGC_coords_unitsphere,0,Inf);
-
 %%
 
 shells = struct;
@@ -220,7 +223,7 @@ accumulators.mCherry = mCherry_accumulator;
 
 % %% COMPUTE ACCUMULATOR
 % if strcmp(p.handledChannel, 'DAPI')
-%     
+%
 %     % make sure to use specific cell radius for dapi cells
 %     if ( isfield(p.option, 'dapiCellradius') )
 %        % p.option.cellradius = p.option.dapiCellradius;
@@ -228,18 +231,18 @@ accumulators.mCherry = mCherry_accumulator;
 %        % p.option.cellradius = 2; % use default size for dapi cell radius
 %     end
 % elseif strcmp(p.handledChannel, 'mCherry')
-%     
+%
 %     %% SLICE WISE PLOTS WITH PROJECTED REFERENCE LANDMARK
 %     %fig_filename_base = [p.resultsPath ,'/heatmaps/'];
 %     %referenceLandmark = computeReferenceLandmark(fileNames,numberOfResults, p);
 %     %createSlicesPlots(accumulator, p.option, 'Number of PGCs', referenceLandmark, [fig_filename_base, 'PGCs_positions'], 1);
-% 
+%
 % else
 %     fprintf('There is no correct channel selected to generate heatmaps!\n');
 %     return;
 % end
 
-%% HANDLE HEATMAPS ( Computation, drawing and saving ) 
+%% HANDLE HEATMAPS ( Computation, drawing and saving )
 handleHeatmaps(accumulators,shells,numberOfResults,p);
 
 %% USER OUTPUT
