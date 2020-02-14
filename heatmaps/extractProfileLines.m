5390816bc3454752325ddf9cbaea7644fa20d5ab
Daniel Tenbrinck <daniel.tenbrinck@fau.de>
2020-02-14 11:43:38 +0100

    Improved location for head profile line extraction.

--- heatmaps/extractProfileLines.m
+++ heatmaps/extractProfileLines.m
@@ -4,7 +4,7 @@ function profileLines = extractProfileLines(mercatorProjections)
 [height, width, shells] = size(mercatorProjections);
 
 % compute profile line locations relative to chosen resolution
-head_location = round(28/90 * width);
+head_location = round(30/90 * width);
 neck_location = round(38/90 * width);
 notochord_location = round(48/90 * width);
 
@@ -14,14 +14,18 @@ profileLines = zeros(height,3,shells);
 % exract profile lines per shell
 for currentShell=1:shells
     
+    if currentShell==5
+        stop = 1;
+    end
+    
     % head
-    profileLines(:,1,currentShell) = mercatorProjections(:,head_location,currentShell);
+    profileLines(:,1,currentShell) = mean(mercatorProjections(:,head_location-1:head_location+1,currentShell),2);
     
     % neck
-    profileLines(:,2,currentShell) = mercatorProjections(:,neck_location,currentShell);
+    profileLines(:,2,currentShell) = mean(mercatorProjections(:,neck_location-1:neck_location+1,currentShell),2);
     
     % notochord
-    profileLines(:,3,currentShell) = mercatorProjections(:,notochord_location,currentShell);
+    profileLines(:,3,currentShell) = mean(mercatorProjections(:,notochord_location-1:notochord_location+1,currentShell),2);
 end
 
 end
