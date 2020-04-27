04605c5a21bc1f438e26086737622bcece8ca5bc
pemlange <you@example.com>
2020-04-27 13:07:06 +0200

    added case for anti flip

--- registration/computeRegression_new.m
+++ registration/computeRegression_new.m
@@ -32,10 +32,21 @@ if pstar(3) < 0 %Zebrafish
     if vstar(1) < 0
     vstar = - vstar;
     end
-else 
+elseif pstar(3) < 0
     if vstar(1) > 0
     vstar = -vstar;
     end
+else
+    disp('pstar  of great circle is zero!')
+    if pstar(1) < 0
+        if vstar(3) > 0
+        vstar = -vstar;
+        end
+    else 
+        if vstar(3) < 0
+        vstar = -vstar;
+        end    
+    end      
 end
     
     
