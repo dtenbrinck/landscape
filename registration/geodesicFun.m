a3c4aa89bbb50bc40c421e6665380194e930d412
PemLange <44814089+PemLange@users.noreply.github.com>
2020-09-23 13:37:45 +0200

    edited commentary
    Thought commentary made no sense?

--- registration/geodesicFun.m
+++ registration/geodesicFun.m
@@ -1,11 +1,7 @@
 function [G] = geodesicFun(p,v)
 %GEODESICFUN Gives the geodesic function G(t). It can compute the points on
 %the spherical line starting at p with direction v.
-%% INPUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   P:      The 3D spherical data set.
-%% OUTPUT %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   F:      The geodesic regression objective function
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 G = @(t) p*cos(t.*norm(v))+v./norm(v)*sin(t.*norm(v));
 
 end
