693803af3fb263146bfb89c2c267cc108564f2e8
JanHuewel <47003263+JanHuewel@users.noreply.github.com>
2020-09-25 16:17:17 +0200

    shortened the code to use the gui functions

--- evaluation.m
+++ evaluation.m
@@ -4,101 +4,27 @@ clear; clc; close all;
 % define root directory
 root_dir = pwd;
 
-% add path for parameter setup
+% add all required scripts
+addpath([root_dir '/auxiliary/']);
+addpath([root_dir '/fitting/']);
+addpath([root_dir '/gui/']);
+addpath([root_dir '/heatmaps/']);
+addpath([root_dir '/io/']);
 addpath([root_dir '/parameter_setup/']);
+addpath([root_dir '/preprocessing/']);
+addpath([root_dir '/registration/']);
+addpath(genpath([root_dir '/segmentation/']));
+addpath([root_dir '/visualization/']);
 
-% load necessary variables
-p = initializeScript('evaluate', root_dir);
+% load necessary variables. Settings can be adjusted here
+p = ParameterTotal();
 
+% Set default search path for results
+resultsPath = [root_dir '/results'];
 
-%% GET FILES TO PROCESS
-
-% check results directory
+% Get the results location from the user
+p.resultsPath = uigetdir(resultsPath,'Please select a results folder to evaluate');
 checkDirectory(p.resultsPath);
 
-% get filenames of STK files in selected folder
-fileNames = getMATfilenames(p.resultsPath);
-fileNames(find(strcmp(fileNames,'ParameterProcessing.mat'))) = [];
-fileNames(find(strcmp(fileNames,'ParameterHeatmap.mat'))) = [];
-% get number of experiments
-numberOfResults = size(fileNames,1);
-
-% check if any results have been found
-if numberOfResults == 0
-    disp('All results already processed or path to results folder wrong?');
-    disp(p.resultsPath);
-    return;
-else
-    disp([ num2str(numberOfResults) ' results found in folder for processing.']);
-end
-
-%% MAIN EVALUATION LOOP
-h = figure('units','normalized','outerposition',[0 0 1 1]); 
-result = 0;
-while result < numberOfResults
-    
-    result = result + 1;
-    % load result data
-    load([p.resultsPath,'/',fileNames{result,1}])
-    
-    % visualize results
-%     visualizeResults(experimentData, processedData, registeredData);
-    visualizeResults_evaluation(h,gatheredData);
-    
-    % display user output
-    fprintf(gatheredData.filename);
-    
-    % ask user to decide what to do with the results
-    choice = questdlg(['What do you want to do with the results of dataset ' gatheredData.filename '?'], ...
-        'Decision on results', ...
-        'Accept','Improve', 'Reject','Accept');
-    
-    % Handle response
-    switch choice
-        case 'Accept'
-            fprintf('\t -> Accepted!\n');
-            movefile([p.resultsPath '/' fileNames{result,1}], [p.resultsPath '/accepted/' fileNames{result,1}]);
-        case 'Improve'
-            
-            % ask user what he wants to improve
-             choice = questdlg(['Which segmentation would you like to improve for ' gatheredData.filename '?'], ...
-        'Choose one option', ...
-        'Landmark','Cells','Cells');
-            
-            if strcmp(choice, 'Cells')
-                modifiedData = gui_manualSegmentation(gatheredData,p,'cells');
-            elseif strcmp(choice, 'Landmark')
-                modifiedData = gui_manualSegmentation(gatheredData,p,'tissue');
-            else
-                error('This should never happen!');
-            end
-            
-            if isempty(modifiedData)
-              fprintf('\t -> Improvement aborted!\n');
-              result = result - 1; % adjust for another try
-              continue;
-            end
-            modifiedData.experiment.filename = modifiedData.filename;
-            saveResults(modifiedData.experiment, modifiedData.processed, modifiedData.registered,...
-                        modifiedData.processed.ellipsoid, modifiedData.registered.transformation_normalization, modifiedData.registered.transformation_rotation,...
-                        [p.resultsPath,'/',fileNames{result,1}] );
-            result = result - 1; % adjust for another try
-            fprintf('\t -> Improved!\n');
-        case 'Reject'
-            fprintf('\t -> Rejected!\n');
-            movefile([p.resultsPath '/' fileNames{result,1}], [p.resultsPath '/rejected/' fileNames{result,1}]);
-      otherwise
-            fprintf('\t -> Evaluation aborted!\n');
-            aborted = true;
-            break;
-    end
-    
-end
-
-%% USER OUTPUT
-if exist('aborted','var')
-  disp('Evaluation script terminated by user.');
-else
-  disp('All results in folder processed!');
-end
-close all;
+% Start intern evaluation script
+evaluation_gui(p);
--- heatmaps_singleExperiment.m
+++ heatmaps_singleExperiment.m
@@ -4,11 +4,30 @@ clear; clc; close all;
 % define root directory
 root_dir = pwd;
 
-% add path for parameter setup
+% add all required scripts
+addpath([root_dir '/auxiliary/']);
+addpath([root_dir '/fitting/']);
+addpath([root_dir '/gui/']);
+addpath([root_dir '/heatmaps/']);
+addpath([root_dir '/io/']);
 addpath([root_dir '/parameter_setup/']);
+addpath([root_dir '/preprocessing/']);
+addpath([root_dir '/registration/']);
+addpath(genpath([root_dir '/segmentation/']));
+addpath([root_dir '/visualization/']);
 
-% load necessary variables
-p = initializeScript('heatmap', root_dir);
+% load necessary variables. Settings can be adjusted here
+p = ParameterTotal();
+
+% Set default search path for results
+resultsPath = [root_dir '/results'];
+
+% Get the results location from the user
+p.resultsPath = uigetdir(resultsPath,'Please select a results folder to generate heatmap');
+p.resultsPathAccepted = [p.resultsPath,'/accepted'];
+if ~exist([p.resultsPath,'/heatmaps'],'dir')
+    mkdir([p.resultsPath,'/heatmaps']);
+end
 
 % generate heatmaps for single experiment
 generateHeatmaps(p);
